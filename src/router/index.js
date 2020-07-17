72be674dbb4c6f22c2f60b7ef7c46af0e1d28fce
Tang <Michael.Tang@gov.bc.ca>
2020-07-17 10:49:03 -0700

    routes added

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,31 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/school-districts',
+    name: 'SchoolDistricts',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/SchoolDistricts.vue')
+  },
+  {
+    path: '/glossary',
+    name: 'Glossary',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Glossary.vue'),
+    props: true
+  },
+  {
+    path: '/glossary',
+    name: 'Glossary',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Glossary.vue'),
+    props: true
+  },
 ]
 
 const router = new VueRouter({
