c39bcec13c63c02e2c59f5181727a0895c4caa92
Lum <Shaun.Lum@gov.bc.ca>
2020-07-17 16:45:22 -0700

    bootstrap vue theme

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="navigation">
-   Navigation 
-   <router-link to="/">Home</router-link>
-   <router-link to="/glossary">Glossary</router-link>
-   <router-link to="/school-district">School District Directory</router-link>
-  </div>
+ <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Student Success</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse is-nav>
+      <b-navbar-nav >
+        <b-nav-item href="#"><router-link to="/">Home</router-link></b-nav-item>
+        <b-nav-item href="#"> <router-link to="/glossary">Glossary</router-link></b-nav-item>
+        <b-nav-item href="#"><router-link to="/school-district">School District Directory</router-link></b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+ 
 </template>
 
 <script>
@@ -18,4 +37,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.nav-link a{ color: white !important;}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <b-card no-block>
-    <b-tabs>
-      <b-tab title="Tab 1">
-        Tab 1 Contents
-      </b-tab>
-      <b-tab title="Tab 2">
-        Tab 2 Contents <b-button size="md">Boop</b-button>
-      </b-tab>
-      <b-tab title="Tab 3" disabled>
-        Tab 3 Contents
-      </b-tab>
-    </b-tabs>
-  </b-card>
   </div>
 
 </template>
