2fc83468960b4012b13e1ae4c296ba9ed60ac1cb
Tang <Michael.Tang@gov.bc.ca>
2020-07-22 09:08:01 -0700

    added schools route

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import Glossary from './views/Glossary.vue'
 import SchoolDistrictDirectory from './views/SchoolDistrictDirectory.vue'
 import SchoolDistrict from './views/SchoolDistrict.vue'
 
-//import School from './views/School.vue'
+import School from './views/School.vue'
 import SchoolDistrictSchoolsDirectory from './views/SchoolDistrictSchoolsDirectory.vue'
 
 export const routes = [
@@ -14,7 +14,8 @@ export const routes = [
     { path:'/school-districts', component: SchoolDistrictDirectory,},
     { path:'/school-district/:did', name: 'schoolDistrictByNumber',component: SchoolDistrict, children:[
         //{ path: '', component: SchoolDistrictDirectory },
-        { path: 'schools', component: SchoolDistrictSchoolsDirectory  }
+        { path: 'schools', component: SchoolDistrictSchoolsDirectory },
+        { path: 'schools/:sid', component: School },
     ]}
   //  { path: '/school-district/:did', name: 'schoolDistrictByNumber', component: SchoolDistrict
   //    children: [
