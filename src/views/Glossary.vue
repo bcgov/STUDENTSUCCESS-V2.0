67e20439cc1024f908ccdbfab867d8ce0194c501
Michael Tang <michael.tang@gov.bc.ca>
2020-07-24 15:10:29 -0700

    glossary

--- src/views/Glossary.vue
+++ src/views/Glossary.vue
@@ -4,7 +4,26 @@
   </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
+  
   name: 'Glossary',
+  created () {
+    axios.get('https://test.studentsuccess.gov.bc.ca/glossary-api')
+      .then(res => {
+          //console.log(res)
+          const data = res.data
+          const titles = []
+          for (let key in data){
+            const title = data[key]
+            title.en_title = key
+            titles.push(title)
+          }
+          // console.log(users)
+          // this.email = users[0].email
+          console.log(titles);
+      })
+      .catch(error => console.log(error))
+  }
 }
 </script>
