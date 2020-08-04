41382efdc8d8eed3224ca174f2f3694e75a6176a
Tang <Michael.Tang@gov.bc.ca>
2020-08-04 09:27:53 -0700

    glossary update

--- src/views/Glossary.vue
+++ src/views/Glossary.vue
@@ -1,30 +1,37 @@
 <template>
   <div class="glossary">
     <h1>Glossary</h1>
-    hello world
+   <!-- <ul v-for="data in glossaryData" :key="city.glossary_item">
+      
+    </ul>-->
   </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  
-  name: 'Glossary',
-  created () {
-    axios.get('https://test.studentsuccess.gov.bc.ca/glossary-api')
-      .then(res => {
-          //console.log(res)
-          const data = res.data
-          const titles = []
-          for (let key in data){
-            const title = data[key]
-            title.en_title = key
-            titles.push(title)
-          }
-          // console.log(users)
-          // this.email = users[0].email
-          console.log(titles);
+  name: "Glossary",
+  data() {
+    return {
+      glossaryData: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://test.studentsuccess.gov.bc.ca/glossary-api")
+      .then((res) => {
+        //console.log(res)
+        this.glossaryData = res.data;
+        //const titles = []
+        //for (let key in data){
+        //  const title = data[key]
+        //  title.en_title = key
+        //  titles.push(title)
+        // }
+        // console.log(users)
+        // this.email = users[0].email
+        console.log(this.glossaryData);
       })
-      .catch(error => console.log(error))
-  }
-}
+      .catch((error) => console.log(error));
+  },
+};
 </script>
